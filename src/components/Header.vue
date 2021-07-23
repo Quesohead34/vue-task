f09835cb75ccc58c84360504c61f5c61469e544e
jordan gillispie <jordangillispie@jordans-MacBook-Pro.local>
2021-07-23 14:43:50 -0700

    added the initial component for todo app

---
+++ src/components/Header.vue
@@ -0,0 +1,36 @@
+// this is the main app components template containing other components //
+<template>
+    <header>
+        <h1>Task Tracker</h1>
+        <Button text= "Add Task" color= "green"/>
+    </header>
+</template>
+
+// this is the main logic that contains imports of related components & functionality //
+
+<script>
+
+import Button from './Button'
+
+export default {
+    name: 'Header',
+    props: {
+        title: String,
+    },
+    components: {
+        Button
+    }
+}
+</script>
+
+// this is the styling for the main component since scoped it will only apply to this component //
+
+
+<style scoped>
+header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 20px;
+}    
+</style>
